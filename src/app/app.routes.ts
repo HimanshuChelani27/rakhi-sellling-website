c3384546fbfc0b32a52a901a96e8c7d911d59bcf
HimanshuChelani27 <himanshuchelani4@gmail.com>
2024-08-10 18:47:23 -0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { CategoryComponent } from './category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path:'details', component: DetailsComponent},
+    {path: 'category', component: CategoryComponent},
+    {path:'', redirectTo:'category', pathMatch:'full'}
+];
+
